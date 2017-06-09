d48418a00a130442198377cbc9bd4e4421890a22
dvdwalt <dvdwalt@e9fd4974-e3b9-4200-b821-f6b099dd8586>
2017-06-09 14:23:58 +0000

    FreeRADIUS 64bit 3.0.14 packages

--- rd/app/model/mAccessPointExit.js
+++
@@ -1,9 +0,0 @@
-Ext.define('Rd.model.mAccessPointExit', {
-    extend: 'Ext.data.Model',
-    fields: [
-         {name: 'id',               type: 'int'     },
-         {name: 'ap_profile_id',    type: 'int'     },
-         {name: 'type',             type: 'string'  },
-         'connects_with'
-        ]
-});
